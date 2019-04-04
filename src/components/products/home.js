41d12a1700f4735e1ad79d86d785ad3e7c706a86
jungiang <jungiang@yahoo.com>
2019-04-04 11:23:21 -0700

    client-nav changes

--- src/components/products/home.js
+++ src/components/products/home.js
@@ -2,12 +2,8 @@ import React from 'react';
 
 export default props => (
     <div className="home">
-        <h1 className="rotateFast">🥑</h1>
+        <h1>🥑</h1>
         <h1 className="center purple-text text-darken-2">Wicked Sales</h1>
-        <h1 className="rotateFast">🥑</h1>
-        <h1 className="rotate">🥑</h1>
-        <h1 className="rotate">🥑</h1>
-        <h1 className="rotate">🥑</h1>
-        <h1 className="rotateSlow">🥑</h1>
+        <h1>🥑</h1>
     </div>
 )
