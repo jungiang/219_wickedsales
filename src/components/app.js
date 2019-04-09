ef50121c6a392b71dcbf5abfec00414f12659379
jungiang <jungiang@yahoo.com>
2019-04-08 17:03:06 -0700

    completed 404 page for client

--- src/components/app.js
+++ src/components/app.js
@@ -2,17 +2,21 @@ import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min'
 import '../assets/css/app.scss';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import ProductRoutes from './products';
 import Home from './products/home';
 import Nav from './nav';
+import NotFound from './404';
 
 const App = () => (
     <div>
         <Nav />
         <div className="container">
-            <Route exact path="/" component={Home}/>
-            <Route path="/products" component={ProductRoutes}/>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/products" component={ProductRoutes}/>
+                <Route component={NotFound}/>
+            </Switch>
         </div>
     </div>
 );
