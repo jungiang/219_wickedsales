3866d97c2905b630c98ef1e005f7a1c242179243
jungiang <jungiang@yahoo.com>
2019-04-12 13:41:00 -0700

    sign in skeleton set

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import App from './components/app';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-    <Router>
-        <App/>
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
